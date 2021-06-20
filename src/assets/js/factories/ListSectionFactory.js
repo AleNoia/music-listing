c98187dc2f3f6b8150a3c38c647b0f6f9cd3ba78
Igor Noia Silva <82424777+AleNoia@users.noreply.github.com>
2021-06-19 23:43:28 -0300

    Preview

--- src/assets/js/factories/ListSectionFactory.js
+++ src/assets/js/factories/ListSectionFactory.js
@@ -4,25 +4,23 @@ function ListSectionFactory() {
         $('.listSection').html('') // Zerando o html para a entrada de uma nova lista
 
         let img // Armazenando o caminho da imagem
-        let previewHtml // Armazenando o caminho do preview da musica
 
         // Validando de acordo com o tipo pois os caminhos no obj são diferentes
         if (type == 'musics') {
             array = array.tracks.data
             img = 'value.album.cover_medium'
-            previewHtml = 'value.preview'
         }
 
         if (type == 'albuns') {
             array = array.albums.data
             img = 'value.cover_medium'
-        } 
+        }
 
         if (type == 'artists') {
             array = array.artists.data
             img = 'value.picture_medium'
         }
-        
+
         if (type == 'podcasts') {
             array = array.podcasts.data
             img = 'value.picture_medium'
@@ -33,22 +31,19 @@ function ListSectionFactory() {
             img = 'value.picture_medium'
         }
 
-        // Inserindo o preview se o tipo for 'musics'
-        function preview(){
-            if(type == musics){
-                return `
-                <audio controls class="preview audioControlTrack">
-                <source src="${eval(previewHtml)}" class="previewTrack" type="audio/mpeg">
-                </audio>
-                `
+        // Verificando se é do tipo musics, se sim, retornará true para o preview
+        function preview() {
+            if (type == 'musics') {
+                return true
             } else {
-                return ''
+                return false
             }
 
         }
 
         // Gerando o card de acordo com o tipo
         $.each(array, function (key, value) {
+            console.log(value)
             $('.listSection').append(`
             <div class="card cardProject mb-3">
             <div class="row no-gutters">
@@ -59,7 +54,19 @@ function ListSectionFactory() {
                 <div class="card-body">
                   <h5 class="card-title">${key + 1}</h5>
                   <h5 class="card-title">${type == 'artists' ? value.name : value.title}</h5>
-                  ${preview()}
+                  ${
+                      (function (){
+                          if(preview()){
+                              return `
+                              <audio controls class="preview audioControlTrack">
+                                <source src="${value.preview}" class="previewTrack" type="audio/mpeg">
+                                </audio>
+                              `
+                          } else {
+                              return ''
+                          }
+                      })()
+                  }
                   <h4 class="subTit">${type == 'artists' ? value.name : value.title}</h4>
                 </div>
               </div>
